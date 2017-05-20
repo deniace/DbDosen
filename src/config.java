b1f745fa77086ee15116f43c2a28e0477d9efd85
deniace <denisupriyatna01@gmail.com>
2017-05-20 13:36:33 +0700

    Tugas Pemrograman  DB Dosen

---
+++ src/config.java
@@ -0,0 +1,25 @@
+//deni supriyatna
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class config {
+    private static Connection mysqlconfig;
+    
+    public static Connection configDB() throws SQLException
+    {
+        try
+        {
+            String url = "jdbc:mysql://localhost:3306/akademik";
+            String user ="root";
+            String pass = "";
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlconfig = DriverManager.getConnection(url,user,pass);
+        }
+        catch(Exception e)
+        {System.err.println("koneksi gagal"+e.getMessage());}
+        
+        return mysqlconfig;
+    }
+    
+}
